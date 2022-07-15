4f3ed7a545c225e38b71df077e801f3e40092c32
iradakhf <iradakhf@code.edu.az>
2022-07-15 10:57:09 +0400

    add all

---
+++ DemoEncap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    internal class DemoEncap
+    {
+        //private variable declared
+        //these can only be accessed by public method of class
+
+        private string StudentName;
+        private int StudentAge;
+
+        //using accessors to get and set the value of studentName
+        public string Name{ get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleApp44
+{
+    class Program
+    {
+        public static void Main(string[] mrgs)
+        {
+            //creating object 
+            DemoEncap demoEncap = new DemoEncap();
+
+            //calls set accessor of the property Age,
+            //and pass "21" as value of the standard field 'value'
+            demoEncap.Age = 21;
+
+            //calls set accessor of the property Name and pass "Irada" as value of the standard field 
+            demoEncap.Name = "Irada";
+
+            //displaying values of the variables
+
+            Console.WriteLine("Name: " + demoEncap.Name);
+            Console.WriteLine("Age : " +demoEncap.Age);
+        }
+    }
+}
